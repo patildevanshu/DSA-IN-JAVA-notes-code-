bd376cc4263f150a65234b10566ac759b130c37d
patildevanshu <152208944+patildevanshu@users.noreply.github.com>
2024-01-15 19:17:11 +0530

    dsa
    revesearray dsa

---
+++ arrays/reversearray.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class reversearray {
+
+    public static void reversearrays( int array[]){
+
+        int first=0, last=array.length - 1;
+
+        while (first<last) {
+            int swap = array[last];
+            array[last]= array[first];
+            array[first]= swap ;
+
+            first++;
+            last--;
+        }
+            
+    }
+
+    public static void main(String[] args) {
+
+        int array[]= {6,5,6,1,2,7,8,9,4,5} ;
+
+        reversearrays(array);
+        
+        for(int i=0; i<array.length ;i++ ){
+            System.out.print(array[i] + " ");
+        }
+
+
+        
+    }
+    
+}
